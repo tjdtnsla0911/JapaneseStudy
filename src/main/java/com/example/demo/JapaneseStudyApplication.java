78b434945b32d263728396c83143976cf6cd85fb
KIM SUNGSOO <62128928+tjdtnsla0911@users.noreply.github.com>
2022-07-16 14:57:09 +0900

    test

---
+++ src/main/java/com/example/demo/JapaneseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaneseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapaneseStudyApplication.class, args);
+	}
+
+}
